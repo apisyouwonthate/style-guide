79f5a6a9e361df45a5a3a8d893dea413cd471965
Phil Sturgeon <67381+philsturgeon@users.noreply.github.com>
2022-06-28 15:35:59 +0100

    feat: brought back request-GET-no-body-oas2

--- styleguide.js
+++ styleguide.js
@@ -10,26 +10,32 @@ import { oas2, oas3 } from "@stoplight/spectral-formats";
 
 export default {
   rules: {
+
+    // Author: Phil Sturgeon (https://github.com/philsturgeon)
     'api-home': {
       description: 'APIs MUST have a root path (`/`) defined.',
       message: 'Stop forcing all API consumers to visit documentation for basic interactions when the API could do that itself.',
-      severity: "warn",
       given: "$.paths",
       then: {
         field: "/",
         function: truthy,
-      }
+      },
+      severity: 'warn',
     },
+
+    // Author: Phil Sturgeon (https://github.com/philsturgeon)
     'api-home-get': {
       description: 'APIs root path (`/`) MUST have a GET operation.',
       message: "Otherwise people won't know how to get it.",
-      severity: "warn",
       given: "$.paths[/]",
       then: {
         field: "get",
         function: truthy,
-      }
+      },
+      severity: 'warn',
     },
+
+    // Author: Phil Sturgeon (https://github.com/philsturgeon)
     'api-health': {
       description: 'APIs MUST have a health path (`/health`) defined.',
       message: 'Creating a `/health` endpoint is a simple solution for pull-based monitoring and manually checking the status of an API.',
@@ -40,11 +46,13 @@ export default {
         function: truthy,
       }
     },
+
+    // Author: Phil Sturgeon (https://github.com/philsturgeon)
     'api-health-format': {
-      description: 'Health path (`/heath`) SHOULD support Health Check Response Format\"',
+      description: 'Health path (`/heath`) SHOULD support Health Check Response Format',
       message: 'Use existing standards (and draft standards) wherever possible, like the draft standard for health checks: https://datatracker.ietf.org/doc/html/draft-inadarei-api-health-check',
       formats: [oas3],
-      severity: "warn",
+      severity: 'warn',
       given: "$.paths.[/health].responses[*].content.*~",
       then: {
         function: enumeration,
@@ -55,10 +63,12 @@ export default {
         }
       }
     },
+
+    // Author: Phil Sturgeon (https://github.com/philsturgeon)
     'paths-kebab-case': {
       description: 'Should paths be kebab-case.',
       message: '{{property}} should be kebab-case (lower case and separated with hyphens)',
-      severity: "warn",
+      severity: 'warn',
       given: "$.paths[*]~",
       then: {
         function: pattern,
@@ -67,9 +77,10 @@ export default {
         }
       }
     },
+
+    // Author: Phil Sturgeon (https://github.com/philsturgeon)
     'no-numeric-ids': {
       description: 'Please avoid exposing IDs as an integer, UUIDs are preferred.',
-      severity: 'error',
       given: '$.paths..parameters[*].[?(@property === "name" && (@ === "id" || @.match(/(_id|Id)$/)))]^.schema',
       then: {
         function: schema,
@@ -90,12 +101,14 @@ export default {
             }
           }
         }
-      }
+      },
+      severity: 'error',
     },
+
+    // Author: Phil Sturgeon (https://github.com/philsturgeon)
     'no-http-basic': {
       description: 'Consider a more secure alternative to HTTP Basic.',
       message: 'HTTP Basic is a pretty insecure way to pass credentials around, please consider an alternative.',
-      severity: 'error',
       given: "$.components.securitySchemes[*]",
       then: {
         field: "scheme",
@@ -103,8 +116,11 @@ export default {
         functionOptions: {
           notMatch: 'basic'
         }
-      }
+      },
+      severity: 'error',
     },
+
+    // Author: Phil Sturgeon (https://github.com/philsturgeon)
     'no-x-headers': {
       description: 'Please do not use headers with X-',
       message: 'Headers cannot start with X-, so please find a new name for {{property}}. More: https://tools.ietf.org/html/rfc6648',
@@ -114,8 +130,11 @@ export default {
         functionOptions: {
           notMatch: '^(x|X)-'
         }
-      }
+      },
+      severity: 'error',
     },
+
+    // Author: Phil Sturgeon (https://github.com/philsturgeon)
     'no-x-response-headers': {
       description: 'Please do not use headers with X-',
       message: 'Headers cannot start with X-, so please find a new name for {{property}}. More: https://tools.ietf.org/html/rfc6648',
@@ -125,34 +144,53 @@ export default {
         functionOptions: {
           notMatch: '^(x|X)-'
         }
-      }
-    },
-    'request-GET-no-body-oas3': {
-      description: 'A `GET` request MUST NOT accept a request body',
+      },
       severity: 'error',
       formats: [oas3],
+    },
+    // Author: Andrzej (https://github.com/jerzyn)
+    'request-GET-no-body-oas2:': {
+      description: 'A `GET` request MUST NOT accept a `body` parameter',
+      given: '$.paths..get.parameters..in',
+      then: {
+        function: pattern,
+        functionOptions: {
+          notMatch: "/^body$/"
+        }
+      },
+      severity: 'error',
+      formats: [oas2],
+    },
+
+    // Author: Andrzej (https://github.com/jerzyn)
+    'request-GET-no-body-oas3': {
+      description: 'A `GET` request MUST NOT accept a request body',
       given: "$.paths..get.requestBody",
       then: {
         function: undefinedFunc,
-      }
-    },
-    'headers-lowercase-case': {
-      description: 'All HTTP headers MUST use Hyphenated-Pascal-Case casing',
+      },
+      formats: [oas3],
       severity: 'error',
+    },
+
+    // Author: Andrzej (https://github.com/jerzyn)
+    'headers-hyphenated-pascal-case': {
+      description: 'All HTTP headers MUST use Hyphenated-Pascal-Case casing',
       given: "$..parameters[?(@.in == 'header')].name",
-      message: 'HTTP headers have the first letter of each word capitalized, and each word should be seperated by a hyphen.',
+      message: 'HTTP headers have the first letter of each word capitalized, and each word should be separated by a hyphen.',
       type: "style",
       then: {
         function: pattern,
         functionOptions: {
           match: '/^([A-Z][a-z0-9]-)*([A-Z][a-z0-9])+/'
         }
-      }
+      },
+      severity: 'error',
     },
+
+    // Author: Andrzej (https://github.com/jerzyn)
     'hosts-https-only-oas2': {
       description: 'ALL requests MUST go through `https` protocol only',
-      severity: 'error',
-      formats: [oas2],
       type: "style",
       message: 'Schemes MUST be https and no other value is allowed.',
       given: "$.schemes",
@@ -168,12 +206,14 @@ export default {
             "maxItems": 1
           }
         }
-      }
+      },
+      severity: 'error',
+      formats: [oas2],
     },
+
+    // Author: Andrzej (https://github.com/jerzyn)
     'hosts-https-only-oas3': {
       description: 'ALL requests MUST go through https:// protocol only',
-      formats: [oas3],
-      severity: 'error',
       message: 'Servers MUST be https and no other protocol is allowed.',
       given: "$.servers..url",
       then: {
@@ -181,22 +221,26 @@ export default {
         functionOptions: {
           match: '/^https:/'
         }
-      }
+      },
+      formats: [oas3],
+      severity: 'error',
     },
+
+    // Author: Andrzej (https://github.com/jerzyn)
     'request-support-json-oas3': {
       description: 'Every request SHOULD support `application/json` media type',
-      formats: [oas3],
-      severity: "warn",
       message: '{{description}}: {{error}}',
       given: "$.paths.[*].requestBody.content[?(@property.indexOf('json') === -1)]^",
       then: {
         function: falsy,
-      }
+      },
+      formats: [oas3],
+      severity: 'warn',
     },
+
+    // Author: Phil Sturgeon (https://github.com/philsturgeon)
     'no-unknown-error-format': {
       description: 'Every error response SHOULD support either RFC 7807 (https://tools.ietf.org/html/rfc6648) or the JSON:API Error format.',
-      formats: [oas3],
-      severity: "warn",
       given: "$.paths.[*]..responses[?(@property.match(/^(4|5)/))].content.*~",
       then: {
         function: enumeration,
@@ -207,8 +251,12 @@ export default {
             "application/problem+json",
           ]
         }
-      }
+      },
+      formats: [oas3],
+      severity: 'warn',
     },
+
+    // Author: Nauman Ali (https://github.com/naumanali-stoplight)
     'no-global-versioning': {
       description: 'Using global versions just forces all your clients to do a lot more work for each upgrade. Please consider using API Evolution instead.',
       message: 'Server URL should not contain global versions',
@@ -220,7 +268,7 @@ export default {
         }
       },
       formats: [oas3],
-      severity: "warn",
+      severity: 'warn',
     }
   }
 };
