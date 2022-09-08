528a50476a692274f7e25e796993f36cf204a9fb
Alex Savage <alex.savage@oneadvanced.com>
2022-09-08 08:30:55 +0100

    feat: add rule for no media types in resources
    New rule to address: https://github.com/apisyouwonthate/style-guide/issues/24

---
+++ __tests__/no-file-extensions-in-paths.ts
@@ -0,0 +1,30 @@
+import { DiagnosticSeverity } from '@stoplight/types';
+import testRule from './__helpers__/helper';
+
+testRule('no-file-extensions-in-paths', [
+  {
+    name: 'valid case',
+    document: {
+      openapi: '3.1.0',
+      info: { version: '1.0' },
+      paths: { 'resources': {} }
+    },
+    errors: [],
+  },
+
+  {
+    name: 'an API definition that is returning a json file',
+    document: {
+      openapi: '3.1.0',
+      info: { version: '1.0' },
+      paths: { 'resources.json': {} }
+    },
+    errors: [
+      {
+        message: 'Paths must not include file extensions such as .json and .xml',
+        path: ['paths', '0', 'resources.json'],
+        severity: DiagnosticSeverity.Error,
+      },
+    ],
+  }
+]);
